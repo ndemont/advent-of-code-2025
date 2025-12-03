f501aab3d1537004af4eb842daafae059c172870
Nastasia Demont <nastasia@demont.me>
2025-12-03 21:19:40 +0100

    day 3 part2

--- src/day3.rs
+++ src/day3.rs
@@ -1,47 +1,6 @@
-use aoc_runner_derive::{aoc, aoc_generator};
+use aoc_runner_derive::aoc;
 
-fn parse1(input: &str) -> usize {
-    input.lines()
-        .map(|line| {
-            let (pos1, val1) = get_max_digit(&line[..line.len() - 1]).unwrap();
-            let (_pos2, val2) = get_max_digit(&line[pos1 + 1..]).unwrap();
-
-            let string_val = format!("{}{}", val1 as char, val2 as char);
-
-            println!("{}", string_val);
-            string_val.parse::<usize>().unwrap()
-        })
-        .sum()
-}
-
-#[aoc_generator(day3)]
-fn parse(input: &str) -> usize {
-    input.lines()
-        .map(|line| {
-            let mut string_val: String = String::new();
-            let mut sliced_line = line;
-            for i in 0..12 {
-                if string_val.len() >= 12 {
-                    break;
-                }
-                if sliced_line.len() <= 12 - i {
-                    let (new_pos, new_val) = get_max_digit(&sliced_line[..12 - i]).unwrap();
-                    sliced_line = &sliced_line[new_pos + 1..];
-                    println!("{}", string_val);
-                    string_val = format!("{}{}", string_val, new_val);
-                } else {
-                    println!("{}", string_val);
-                    string_val = format!("{}{}", string_val, &sliced_line[..12 - string_val.len()]);
-                    break;
-                }
-            }
-            println!("{}", string_val);
-            string_val.parse::<usize>().unwrap()
-        })
-        .sum()
-}
-
-fn get_max_digit(slice: &str) -> Option<(usize, u8)> {
+fn get_max_batterie(slice: &str) -> Option<(usize, u8)> {
     slice.as_bytes()
         .iter()
         .enumerate()
@@ -50,19 +9,46 @@ fn get_max_digit(slice: &str) -> Option<(usize, u8)> {
         .map(|(index, &byte)| (index, byte))
 }
 
+fn get_max_joltage(size: usize, bank: &str) -> usize {
+    let mut remaining_bank = bank;
+    // with capacity helpps to avoid reallocation, in our case we can reuse the same space
+    let mut max_batteries = String::with_capacity(size);
+
+    for i in 0..size {
+        let len = remaining_bank.len();
+        let remaining_batteries = size - i;
+        let search_range = len - remaining_batteries;
+        let searchable_bank = &remaining_bank[..=search_range];
+
+        if let Some((pos, batterie)) = get_max_batterie(searchable_bank) {
+            max_batteries.push(batterie as char);
+            remaining_bank = &remaining_bank[pos + 1..];
+        } else {
+            break;
+        }
+    }
+
+    max_batteries.parse().unwrap()
+}
+
 #[aoc(day3, part1)]
-fn part1(input: &usize) -> usize {
-    *input
+fn part1(input: &str) -> usize {
+    input.lines()
+        .map(|line| get_max_joltage(2, line))
+        .sum()
 }
 
 #[aoc(day3, part2)]
-fn part2(input: &usize) -> usize {
-    *input
+fn part2(input: &str) -> usize {
+    input.lines()
+        .map(|line| get_max_joltage(12, line))
+        .sum()
 }
 
-const INPUT: &str = "811111111111119
+const INPUT: &str = "818181911112111
+811111111111119
 234234234234278
-818181911112111";
+987654321111111";
 
 #[cfg(test)]
 mod tests {
@@ -70,11 +56,13 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse1(INPUT)), 357);
+        assert_eq!(part1(INPUT), 357);
     }
+    // real input answer 17166
 
     #[test]
     fn part2_example() {
-        assert_eq!(part1(&parse(INPUT)), 3121910778619);
+        assert_eq!(part2(INPUT), 3121910778619);
     }
+    // real input answer 169077317650774
 }
