1998657d39f4dacdeb1c4d663729a7088f438669
Nastasia Demont <nastasia.demont@doctolib.com>
2025-12-03 18:26:02 +0100

    tmp part2

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,6 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-#[aoc_generator(day3)]
-fn parse(input: &str) -> usize {
+fn parse1(input: &str) -> usize {
     input.lines()
         .map(|line| {
             let (pos1, val1) = get_max_digit(&line[..line.len() - 1]).unwrap();
@@ -9,6 +8,33 @@ fn parse(input: &str) -> usize {
 
             let string_val = format!("{}{}", val1 as char, val2 as char);
 
+            println!("{}", string_val);
+            string_val.parse::<usize>().unwrap()
+        })
+        .sum()
+}
+
+#[aoc_generator(day3)]
+fn parse(input: &str) -> usize {
+    input.lines()
+        .map(|line| {
+            let mut string_val: String = String::new();
+            let mut sliced_line = line;
+            for i in 0..12 {
+                if string_val.len() >= 12 {
+                    break;
+                }
+                if sliced_line.len() <= 12 - i {
+                    let (new_pos, new_val) = get_max_digit(&sliced_line[..12 - i]).unwrap();
+                    sliced_line = &sliced_line[new_pos + 1..];
+                    println!("{}", string_val);
+                    string_val = format!("{}{}", string_val, new_val);
+                } else {
+                    println!("{}", string_val);
+                    string_val = format!("{}{}", string_val, &sliced_line[..12 - string_val.len()]);
+                    break;
+                }
+            }
             println!("{}", string_val);
             string_val.parse::<usize>().unwrap()
         })
@@ -29,13 +55,12 @@ fn part1(input: &usize) -> usize {
     *input
 }
 
-// #[aoc(day3, part2)]
-// fn part2(input: &str) -> String {
-//     todo!()
-// }
+#[aoc(day3, part2)]
+fn part2(input: &usize) -> usize {
+    *input
+}
 
-const INPUT: &str = "987654321111111
-811111111111119
+const INPUT: &str = "811111111111119
 234234234234278
 818181911112111";
 
@@ -45,11 +70,11 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse(INPUT)), 357);
+        assert_eq!(part1(&parse1(INPUT)), 357);
     }
-    //
-    // #[test]
-    // fn part2_example() {
-    //     assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
-    // }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part1(&parse(INPUT)), 3121910778619);
+    }
 }
